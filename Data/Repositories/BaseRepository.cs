8ac2c1f76eec10f304153b060d8e6d849aac9850
Elin Reckermann <elin89.ehr@gmail.com>
2025-04-18 10:40:52 +0200

    Base repository added

---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,93 @@
+﻿using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Data.Repositories;
+
+public abstract class BaseRepository<TEntity> where TEntity : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<TEntity> _table;
+
+    protected BaseRepository(AppDbContext context)
+    {
+        _context = context;
+        _table = context.Set<TEntity>();
+    }
+
+    public virtual async Task<bool> AddAsync(TEntity entity) //public virtual async?? eller går dte även med bara public async
+    {
+        if (entity == null)
+        {
+            return false;
+        }
+        try
+        {
+            _table.Add(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        var entities = await _table.ToListAsync();
+        return entities;
+    }
+
+    public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> findBy)
+    {
+        var entity = await _table.FirstOrDefaultAsync(findBy);
+        return entity ?? null!;
+    }
+
+    public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> findBy)
+    {
+        var exists = await _table.AnyAsync(findBy);
+        return exists;
+    }
+
+    public virtual async Task<bool> UpdateAsync(TEntity entity) //public virtual async?? eller går det även med bara public async
+    {
+        if (entity == null)
+        {
+            return false;
+        }
+        try
+        {
+            _table.Update(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public virtual async Task<bool> DeleteAsync(TEntity entity) //public virtual async?? eller går dte även med bara public async
+    {
+        if (entity == null)
+        {
+            return false;
+        }
+        try
+        {
+            _table.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+}
